15a8640b828643ee744ad73cff58abc1a227d64d
Vasyl <vzdendyak@gmail.com>
2020-12-22 08:16:34 +0200

    Generated client-side

--- restaurant-client/src/app/app.module.ts
+++ restaurant-client/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu/menu.component';
+import { MenuListComponent } from './menu/menu-list/menu-list.component';
+import { DishInfoComponent } from './dish/dish-info/dish-info.component';
+import { IngredientListComponent } from './ingredient/ingredient-list/ingredient-list.component';
+import { OrderComponent } from './order/order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MenuListComponent,
+    DishInfoComponent,
+    IngredientListComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
---
+++ restaurant-client/src/app/dish/dish-info/dish-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dish-info',
+  templateUrl: './dish-info.component.html',
+  styleUrls: ['./dish-info.component.scss']
+})
+export class DishInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
